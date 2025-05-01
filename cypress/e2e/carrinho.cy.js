48b55b1ce9b70a9e262863b2237e889b8603a792
igorpaes21 <irsp2121@hmail.com>
2025-04-30 23:24:42 -0300

    Inserção do comando para increenshot das telas e implementação da lógica para testes de remoção de produto do carrinh

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -13,6 +13,8 @@ describe('Carrinho', () => {
         //Act
         cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
 
+        cy.screenshot('adicionar_produto_carrinho.png')
+
         //Assert
         cy.get('.shopping_cart_badge').should('be.visible').and('have.text','1')
 
@@ -21,4 +23,32 @@ describe('Carrinho', () => {
         cy.contains('Sauce Labs Backpack').should('be.visible')
 })
 
+    it('Remover produtos do carrinho com sucesso', () => {
+        //Arrage
+        cy.visit('https://www.saucedemo.com/')
+
+        cy.get('[data-test="username"]').type('problem_user')
+
+        cy.get('[data-test="password"]').type('secret_sauce')
+
+        cy.get('[data-test="login-button"]').click()
+
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+        cy.get('[.shopping_cart_badge]').should('be.visible')
+
+        //Act
+        cy.get('[data-test="remove-sauce-labs-backpack"]').click()
+
+        .cy.screenshot('remover_produto_carrinho.png')
+
+        
+
+        cy.screenshot('remover_produto_carrinho.png')
+
+        //Assert
+        cy.get('[data-test="remove-sauce-labs-backpack"]').click()
+
+        cy.get('.shopping_cart_badge').should('not.exist')
+    })
 })
